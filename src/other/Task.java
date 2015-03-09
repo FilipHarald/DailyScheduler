dabca4e074b93db5b6f3ba952c1d44e6a80cee5a
FilipHarald <filip.harald@gmail.com>
2015-03-09 15:00:59 +0100

    Min första commit, har lagt till skelettet för projektet samt lagt till eclipse filer i .gitignore

---
+++ src/other/Task.java
@@ -0,0 +1,5 @@
+package other;
+
+public class Task {
+
+}
---
+++ src/visaAttGitFungerar/Filip.java
@@ -0,0 +1,5 @@
+package visaAttGitFungerar;
+
+public class Filip {
+//Jag har lyckats pulla, ändra och pusha vårt projekt. gör en egen klass om du kan det med.
+}
