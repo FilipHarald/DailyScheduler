b509b16fc01b6e266a09b4c9bae6dcf69eba1fb1
frenorell <f_norell@hotmail.com>
2015-03-13 14:12:27 +0100

    JButton tillagd, GUI upplägg fixat.

--- src/network/client/GUI/GUIDailySchedulerPanel.java
+++ src/network/client/GUI/GUIDailySchedulerPanel.java
@@ -26,46 +26,48 @@ public static void main(String[] args){
 }
 
 	public GUIDailySchedulerPanel() {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-				} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-					ex.printStackTrace();
-				}
+		   EventQueue.invokeLater(new Runnable() {
+	            @Override
+	            public void run() {
+	                try {
+	                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+	                    ex.printStackTrace();
+	                }
 
-				JPanel form = new JPanel(new GridBagLayout());
+	                JPanel form = new JPanel(new GridBagLayout());
 
-				JLabel name = new JLabel("Dick Size: ");
-				//	        JLabel age = new JLabel("Age: ");
+	                JLabel name = new JLabel("Användarnamn: ");
+//	        JLabel age = new JLabel("Age: ");
 
-				JTextField field1 = new JTextField(10);
-				//	        JTextField field2 = new JTextField(3);
+	                JTextField field1 = new JTextField(10);
+//	        JTextField field2 = new JTextField(3);
+	            	JButton logIn = new JButton("Logga in");
 
-				GridBagConstraints gbc = new GridBagConstraints();
-				gbc.gridx = 0;
-				gbc.gridy = 0;
-				gbc.anchor = GridBagConstraints.WEST;
+	                GridBagConstraints gbc = new GridBagConstraints();
+	                gbc.gridx = 0;
+	                gbc.gridy = 0;
+	                gbc.anchor = GridBagConstraints.WEST;
 
-				form.add(name, gbc);
-				gbc.gridy++;
-				//	                form.add(age, gbc);
+	                form.add(name, gbc);
+	                gbc.gridy++;
+	                form.add(logIn, gbc);
 
-				gbc.gridx++;
-				gbc.gridy = 0;
-				form.add(field1, gbc);
-				gbc.gridy++;
-				//	                form.add(field2, gbc);
-				
-				JFrame frame = new JFrame("Testing");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-                frame.add(form);
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-			} 
-		});
-	}
+	                gbc.gridx++;
+	                gbc.gridy = 0;
+	                form.add(field1, gbc);
+	                gbc.gridy++;
+	                form.add(logIn, gbc);
+
+	                JFrame frame = new JFrame("Testing");
+	                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	                frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	                frame.add(form);
+	                frame.pack();
+	                frame.setLocationRelativeTo(null);
+	                frame.setVisible(true);
+	            }
+	        });
+	    }
+
 }
