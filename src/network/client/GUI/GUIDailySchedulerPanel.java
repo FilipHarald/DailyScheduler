a484728374a487301027219d2af10b00cb5fb8a5
frenorell <f_norell@hotmail.com>
2015-03-13 13:06:35 +0100

    Rätt storlek på textfield och JLabel. Nästa steg: Actionlistener.

--- src/network/client/GUI/GUIDailySchedulerPanel.java
+++ src/network/client/GUI/GUIDailySchedulerPanel.java
@@ -2,58 +2,70 @@ package network.client.GUI;
 
 import java.awt.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
+import javax.swing.border.Border;
 
 public class GUIDailySchedulerPanel extends JPanel{
-//	private JPanel pnlCenter = new JPanel (new BorderLayout());
-//	private JPanel pnlSouth = new JPanel (new BorderLayout());
-	private JPanel pnlCenterCenter = new JPanel (new BorderLayout());
-	
+//	private JPanel test = new JPanel(new BorderLayout());
+//
+//
+//	private JPanel pnlInloggning = new JPanel (new GridLayout (2,2));
+//
+//
+//	private JLabel lblName = new JLabel ("Användaramn: ");
+//	private JTextField tfName = new JTextField(10);
+//
+//	private JLabel lblPass = new JLabel ("Lösenord: ");
+//	private JTextField tfPass = new JTextField(10);
+//
+//	private JButton btnSummary = new JButton("Logga in");
 
-	private JPanel pnlInloggning = new JPanel (new GridLayout (2,2));
-	
-	
-	private JLabel lblName = new JLabel ("Användaramn: ");
-	private JTextField tfName = new JTextField();
-	
-	private JLabel lblPass = new JLabel ("Lösenord: ");
-	private JTextField tfPass = new JTextField();
-	
-	private JButton btnSummary = new JButton("Logga in");
-	
-	
-	
+
+public static void main(String[] args){
+	new GUIDailySchedulerPanel();
+}
+
 	public GUIDailySchedulerPanel() {
-		setPreferredSize(new Dimension (250,200));
-		setLayout(new BorderLayout());
-		
-		
-		
-		
-		pnlCenterCenter.setBorder( BorderFactory.createTitledBorder( "Inloggning" ) );
-		pnlCenterCenter.add(pnlInloggning, BorderLayout.CENTER );
-		pnlCenterCenter.add(btnSummary, BorderLayout.SOUTH);
-		
-		lblPass.setPreferredSize(new Dimension(50,50));
-		tfPass.setPreferredSize(new Dimension(50,50));
-		lblName.setPreferredSize(new Dimension(50,50));
-		tfName.setPreferredSize(new Dimension(50,50));
-		
-		
-		pnlInloggning.add(lblName);
-		pnlInloggning.add(tfName);
-		pnlInloggning.add(lblPass);
-		pnlInloggning.add(tfPass);
-		
-		add(pnlCenterCenter,BorderLayout.CENTER);
-		
-		
-		
-	} 
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+					ex.printStackTrace();
+				}
 
+				JPanel form = new JPanel(new GridBagLayout());
+
+				JLabel name = new JLabel("Dick Size: ");
+				//	        JLabel age = new JLabel("Age: ");
+
+				JTextField field1 = new JTextField(10);
+				//	        JTextField field2 = new JTextField(3);
+
+				GridBagConstraints gbc = new GridBagConstraints();
+				gbc.gridx = 0;
+				gbc.gridy = 0;
+				gbc.anchor = GridBagConstraints.WEST;
+
+				form.add(name, gbc);
+				gbc.gridy++;
+				//	                form.add(age, gbc);
+
+				gbc.gridx++;
+				gbc.gridy = 0;
+				form.add(field1, gbc);
+				gbc.gridy++;
+				//	                form.add(field2, gbc);
+				
+				JFrame frame = new JFrame("Testing");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                frame.add(form);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+			} 
+		});
+	}
 }
