c195809d414bd9f4fa71886fed7e95259bdc51c6
FilipHarald <filip.harald@gmail.com>
2015-03-24 14:20:59 +0100

    Basen i Team och lite i User

--- src/team/Team.java
+++ src/team/Team.java
@@ -6,10 +6,40 @@ import user.User;
 
 public class Team {
 	private LinkedList<User> managers;
-	private LinkedList<User> teamMembers;
-	
-	public Team(){
+	private LinkedList<User> members;
+	private String name;
+
+	public Team(String name) {
 		managers = new LinkedList<User>();
-		teamMembers = new LinkedList<User>();
+		members = new LinkedList<User>();
+		this.name = name;
+	}
+
+	public LinkedList<User> getManagers() {
+		return managers;
+	}
+
+	public LinkedList<User> getAllMembers() {
+		return members;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public boolean addMember(User member){
+		return members.add(member);
+	}
+	
+	public boolean addManager(User manager){
+		return members.add(manager);
+	}
+
+	public boolean removeManager(User manager) {
+		return managers.remove(manager);
+	}
+
+	public boolean removeMember(User member) {
+		return managers.remove(member);
 	}
 }
--- src/user/User.java
+++ src/user/User.java
@@ -1,5 +1,12 @@
 package user;
 
 public class User {
+	private boolean isAdmin;
+	private String name;
+	
+	public User(String name, boolean isAdmin){
+		this.name = name;
+		this.isAdmin = isAdmin;
+	}
 
 }
