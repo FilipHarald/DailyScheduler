659f1849f9b288fddcd9e178876c8e1610cd41b8
henkeklein <henrikklein@hotmail.com>
2015-03-24 13:40:39 +0100

    Börjat på Task klassen

--- src/other/Task.java
+++ src/other/Task.java
@@ -1,5 +1,37 @@
 package other;
 
+import java.util.Calendar;
+import user.User;
+
 public class Task {
 
+	private User [] assignees;
+	private String description;
+	private User author;
+	private Calendar calendar;
+	
+	public Task (User [] assignes, User author, Calendar calendar, String description) {
+		this.assignees = assignes;
+		this.author = author;
+		this.calendar = calendar;
+		this.description = description;
+	}
+	
+	public User [] getAssignes() {
+		return assignees;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public User getManager() {
+		return author;
+	}
+	
+	public Calendar getCalender() {
+		return calendar;
+	}
+	
+	
 }
