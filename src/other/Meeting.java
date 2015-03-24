309170b18447a8652eef9a436bfc8e705f2aa506
henkeklein <henrikklein@hotmail.com>
2015-03-24 14:35:50 +0100

    Lagt till klassen Meeting med date

--- src/other/Meeting.java
+++ src/other/Meeting.java
@@ -1,5 +1,31 @@
 package other;
 
-public class Meeting {
+import java.util.Date;
+
+public class Meeting{
+	
+	private String description;
+	private Date date;
+	private long time;
+	
+	public Meeting (String description, Date date) {
+		this.description = description;
+		this.date = date;
+		long time = date.getTime();
+		
+	}
+	
+	public String getDescription () {
+		return description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
+	public long getTime (){
+		return time;
+	}
+	
 
 }
