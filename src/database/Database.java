55ef435d856b07fb23214c84cb12a2296ed797ca
henkeklein <henrikklein@hotmail.com>
2015-03-25 11:47:14 +0100

    Börjat på sparandet av en user

--- src/database/Database.java
+++ src/database/Database.java
@@ -2,11 +2,19 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.mysql.jdbc.Statement;
+
+import user.User;
 
 
 public class Database {
 
+	Connection connect;
+	ArrayList <User> userlist = new ArrayList <User>();
 	public static void connection () {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -16,11 +24,10 @@ public class Database {
 		}
 	}
 	
-	public static void connectionToMysql (){
+	public void connectionToMysql (){
 		connection();
 		String host = "jdbc:mysql://localhost/test";
-		String username = "root";
-		String password = "";
+		String username = "root", password = "";
 		try {
 			Connection connect = DriverManager.getConnection(host, username, password);
 			System.out.println ("It worked");
@@ -29,8 +36,30 @@ public class Database {
 		}
 	}
 	
+	public void sendToDb (){
+		Statement st;
+		try {
+			st = (Statement) connect.createStatement();
+			ResultSet srs = st.executeQuery("Select * from table_user");
+			
+			while (srs.next()){
+				User user = new User(null, false);
+				user.setName (srs.getString("name"));
+				user.setTitle (srs.getString("title"));
+				userlist.add(user);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+				}
+		}
+	
+	
 	
 	public static void main (String [] args) {
-		connectionToMysql();
+		
+		Database db = new Database();
+		db.connectionToMysql();
+		db.sendToDb();
+		
 	}
 }
