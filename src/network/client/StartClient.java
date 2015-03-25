fbb1eb26ef9d31bca8475e1d9d6488015f7ea006
FilipHarald <filip.harald@gmail.com>
2015-03-25 15:04:29 +0100

    grund för login till main-frame

--- src/network/client/StartClient.java
+++ src/network/client/StartClient.java
@@ -3,8 +3,7 @@ package network.client;
 public class StartClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ClientController clientController = new ClientController();
 	}
 
 }
