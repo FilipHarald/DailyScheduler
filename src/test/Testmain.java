a61840725edb10a0691b1af8bd04f285207ef30e
frenorell <f_norell@hotmail.com>
2015-03-25 13:22:27 +0100

    Test för GUI-hantering av användare vid inloggning.

---
+++ src/test/Testmain.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Testmain {
+
+
+		
+		public static void main(String[] args) {
+			TestLog frameTabel = new TestLog();
+		}
+		// TODO Auto-generated constructor stub
+	
+
+}
