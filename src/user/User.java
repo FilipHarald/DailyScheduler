4c1c07f3df8d7c576de55863197dca69a05af906
FilipHarald <filip.harald@gmail.com>
2015-03-25 12:46:37 +0100

    Finished User class, it's very simple

--- src/user/User.java
+++ src/user/User.java
@@ -3,10 +3,22 @@ package user;
 public class User {
 	private boolean isAdmin;
 	private String name;
-	
-	public User(String name, boolean isAdmin){
+
+	public User(String name, boolean isAdmin) {
 		this.name = name;
 		this.isAdmin = isAdmin;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 }
