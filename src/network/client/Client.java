55a0d64fde59c28e55d1da0c358a42ff6457623d
ayaabutaleb <abutalebaya@gmail.com>
2015-03-25 12:46:34 +0100

    updated client

--- src/network/client/Client.java
+++ src/network/client/Client.java
@@ -10,7 +10,7 @@ import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 public class Client {
 
     private String username;
-    private String password;
+    //private String password;
     private Socket socket;
     private ObjectInputStream is;
     private ObjectOutputStream os;
@@ -18,26 +18,36 @@ public class Client {
    
 
     //constructor manages communication between client and server
-    public Client(String ip, int port, String username, String password) throws IOException {
+    public Client(String ip, int port, String username) throws IOException {
         socket = new Socket(ip, port);
         is = new ObjectInputStream(socket.getInputStream());
         os = new ObjectOutputStream(socket.getOutputStream());
         this.username = username;
-        this.password = password;
         new Listener().start();
 
     }
     public String getUsername(){
         return username;
-        
+          
     }
-    public String getPassword(){
-        return password;
-
-    }
+    
     private class Listener extends Thread{
         public void run(){
-            Object object;
+            try{
+                os.writeUTF(username);
+                os.flush();
+            
+            while(true){
+                try{
+                    String s = (String) is.readObject();
+                }
+                catch(IOException e){
+                    System.out.println("server has lost connection: " + e);
+                }
+            }
+            }catch(Exception e){
+                
+            }
         }
           
     }
