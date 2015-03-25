9dc6b64a7208c52801ae1d129976ef92647835b5
FilipHarald <filip.harald@gmail.com>
2015-03-25 12:41:53 +0100

    Finished Team class

--- src/team/Team.java
+++ src/team/Team.java
@@ -1,30 +1,66 @@
 package team;
 
+import java.util.Collections;
 import java.util.LinkedList;
 
 import user.User;
 
+/**
+ * Representation of a team. The team can have more than one manager. Managers and members are separate lists.
+ * The lists are synchronized separately.
+ * 
+ * @author Filip Harald
+ *
+ */
+
 public class Team {
 	private LinkedList<User> managers;
 	private LinkedList<User> members;
+	private int teamSize;
 	private String name;
 
 	public Team(String name) {
-		managers = new LinkedList<User>();
-		members = new LinkedList<User>();
+		managers = (LinkedList<User>) Collections.synchronizedList(new LinkedList<User>());
+		members = (LinkedList<User>) Collections.synchronizedList(new LinkedList<User>());
 		this.name = name;
+		teamSize = 0;
 	}
-
-	public LinkedList<User> getManagers() {
-		return managers;
-	}
-
-	public LinkedList<User> getAllMembers() {
-		return members;
-	}
-
-	public String getName() {
+	
+	public String getTeamName() {
 		return name;
+	}
+	
+	public int getTeamSize(){
+		return teamSize;
+	}
+
+	/**
+	 * @return An array of the managers in the team.
+	 */
+	public User[] getManagers() {
+		return (User[]) managers.toArray();
+	}
+
+	/**
+	 * @return An array of the members(not the managers) in the team.
+	 */
+	public User[] getMembers() {
+		return (User[]) members.toArray();
+	}
+	
+	/**
+	 * @return An array of all the members in the team.
+	 */
+	public User[] getTeam(){
+		User[] temp = new User[teamSize];
+		int counter = 0;
+		for(User member : members){
+			temp[counter++] = member; 
+		}
+		for(User manager : managers){
+			temp[counter++] = manager; 
+		}
+		return temp;
 	}
 	
 	public boolean addMember(User member){
@@ -33,6 +69,14 @@ public class Team {
 	
 	public boolean addManager(User manager){
 		return members.add(manager);
+	}
+	
+	public boolean hasMember(User member){
+		return members.contains(member);
+	}
+	
+	public boolean hasManager(User manager){
+		return members.contains(manager);
 	}
 
 	public boolean removeManager(User manager) {
