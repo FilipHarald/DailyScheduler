bdf6e36cfda7ec8ad32e211022e6afaa7bd53930
ayaabutaleb <abutalebaya@gmail.com>
2015-04-02 10:00:08 +0200

    lade till startApplication o startClient

--- src/network/client/ClientController.java
+++ src/network/client/ClientController.java
@@ -6,7 +6,7 @@
 package network.client;
 
 import java.io.IOException;
-import network.client.GUI.GUIDailySchedulerPanel;
+import network.client.GUI.GUIDailySchedulerApp;
 
 /**
  *
@@ -14,46 +14,43 @@ import network.client.GUI.GUIDailySchedulerPanel;
  */
 public class ClientController {
     private Client client;
-    private GUIDailySchedulerPanel GUI;
+    private GUIDailySchedulerApp GUI;
     private String ip;
     private int port;
     
     
     //constructor sets ip and port
-    public ClientController(){
-    	//1. HÄR SKA LOGIN FÖNSTRET STARTAS
+    public ClientController(String serverIp, int port){
+        ip = serverIp;
+        this.port = port;
+        
     }
     
+    //start application
+    public void startApplication(GUIDailySchedulerApp GUI, String ID){
+        this.GUI = GUI;
+        startClient(ID);
+        
+    }
+    
+    //start client for the user
+     private void startClient(String ID){
+        try{
+            client = new Client(ip, port, ID);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+    }
+   
+        
     //2. DENNA METOD SKA ANROPAS AV LOGINFÖNSTRET
     public void logIn(String userName, String password){
     	if(userName.equals("test") && password.equals("12345")){
     		//3. HÄR SKA MAINFÖNSTRETSKAPAS
     	}
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //start client for the given username
-    private void startClient(String username){
-        try {
-            client = new Client(ip, port, username);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-    
+
     
     //disconnect client when user logs out
     public void logout(){
