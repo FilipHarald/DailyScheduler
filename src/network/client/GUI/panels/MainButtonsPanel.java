e03f736e27a5739a3645cf991f39457a45323ae3
FilipHarald <filip.harald@gmail.com>
2015-04-09 12:50:11 +0200

    gjort klart MainButtonsPanel och switchsats för knapparna i ApplicationGUI

--- src/network/client/GUI/panels/MainButtonsPanel.java
+++ src/network/client/GUI/panels/MainButtonsPanel.java
@@ -17,21 +17,39 @@ public class MainButtonsPanel extends JPanel {
 	private JButton salariesButton;
 	private JButton logOutButton;
 
-	public MainButtonsPanel(ApplicationGUI applicationGUI){
+	public MainButtonsPanel(ApplicationGUI applicationGUI) {
 		super();
 		this.applicationGUI = applicationGUI;
 		setBorder(BorderFactory.createTitledBorder("Menu"));
 		setPreferredSize(new Dimension(200, 10));
-		
+		addButtons();
+	}
+
+	public void addButtons() {
 		mainButton = new JButton("Main Screen");
 		taskButton = new JButton("Tasks");
 		messageButton = new JButton("Messages");
 		calendarButton = new JButton("Calendar");
 		salariesButton = new JButton("Salaries");
 		logOutButton = new JButton("Log out");
-		logOutButton.setFont(logOutButton.getFont().deriveFont(Font.BOLD | Font.ITALIC));
-		
+
+		logOutButton.setFont(logOutButton.getFont().deriveFont(
+				Font.BOLD | Font.ITALIC));
+
+		mainButton.setActionCommand("main");
+		taskButton.setActionCommand("task");
+		messageButton.setActionCommand("message");
+		calendarButton.setActionCommand("calendar");
+		salariesButton.setActionCommand("salaries");
+		logOutButton.setActionCommand("log out");
+
 		mainButton.addActionListener(new ButtonListener());
+		taskButton.addActionListener(new ButtonListener());
+		messageButton.addActionListener(new ButtonListener());
+		calendarButton.addActionListener(new ButtonListener());
+		salariesButton.addActionListener(new ButtonListener());
+		logOutButton.addActionListener(new ButtonListener());
+
 		add(mainButton);
 		add(taskButton);
 		add(messageButton);
@@ -39,21 +57,22 @@ public class MainButtonsPanel extends JPanel {
 		add(salariesButton);
 		add(logOutButton);
 	}
-	private class ButtonListener implements ActionListener{
+
+	private class ButtonListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			String actionString = e.getActionCommand();
-			
-			switch (actionString) {
-            	case "Log out":	System.out.println("lolol");
-                    	break;
-            	case "lol": actionString = "Maasfin Screen";
-            			break;
-            	default: actionString = "Invalid month";
-            			break;
+
+			String command = e.getActionCommand();
+			switch (command) {
+			case "log out":
+				applicationGUI.logOut();
+				break;
+			default:
+				applicationGUI.changePanel(command);
+				break;
 			}
 		}
-		
+
 	}
 }
