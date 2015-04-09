1a89d252378025f22e316e31aeebba2229f9c218
henkeklein <henrikklein@hotmail.com>
2015-04-09 15:53:24 +0200

    Jävligt snyggt TaskPanel GUI

--- src/network/client/GUI/panels/TaskPanel.java
+++ src/network/client/GUI/panels/TaskPanel.java
@@ -5,65 +5,80 @@ import javax.swing.*;
 
 public class TaskPanel extends JPanel {
 	
-	private JPanel labelpanel = new JPanel (new GridLayout(1,1));
-	private JPanel labelinputpanel = new JPanel (new GridLayout(1,1));
-	private JPanel labelcombobtnpanel = new JPanel (new GridLayout(1,1));
-
+	private JScrollPane listCompletedTask = new JScrollPane ();
+	private JScrollPane listIncompletedTask = new JScrollPane ();
+	private JTextArea textDescription = new JTextArea();
+	
 	
 	public TaskPanel(){
 		super();
 		setLayout (new BorderLayout());
 		setBorder(BorderFactory.createTitledBorder("Task view"));
-
-		AddLabels();
+		AddTasks();
 		AddButtons();
 		AddTextArea();
 		
 	}
 	
+	public void AddTasks() {
+		JPanel tasksPanel = new JPanel(new GridLayout(0, 2));
+		tasksPanel.setPreferredSize(new Dimension(300, 200));
+		tasksPanel.setBorder(BorderFactory.createTitledBorder(""));
+		
+		JPanel panelCompletedTask = new JPanel ();
+		panelCompletedTask.setPreferredSize(new Dimension(200, 100));
+		
+		JLabel lblCompletedTask = new JLabel ("Completed tasks");
+		
+		panelCompletedTask.setBorder(BorderFactory.createTitledBorder(""));
+		listCompletedTask.setPreferredSize(new Dimension(200,100));
+		
+		panelCompletedTask.add(lblCompletedTask);
+		panelCompletedTask.add(listCompletedTask);
+		
+		
+		
+		JPanel panelIncompletedTask = new JPanel ();
+		panelIncompletedTask.setPreferredSize(new Dimension(200, 100));
+
+		JLabel lblIncompleted = new JLabel ("Incompleted tasks");
+		
+		panelIncompletedTask.setBorder(BorderFactory.createTitledBorder(""));
+		listIncompletedTask.setPreferredSize(new Dimension(200,100));
+	
+		panelIncompletedTask.add(lblIncompleted);
+		panelIncompletedTask.add(listIncompletedTask);
+		
+		tasksPanel.add (panelIncompletedTask);
+		tasksPanel.add(panelCompletedTask);
+		
+		add (tasksPanel, BorderLayout.NORTH);
+	}
+	
 	public void AddButtons() {
-		labelcombobtnpanel.setSize(5, 5);
-		labelcombobtnpanel.setPreferredSize(new Dimension(5,5));
+		JPanel labelBtnPanel = new JPanel ();
+		labelBtnPanel.setPreferredSize(new Dimension(5,5));
 	
 		
-		JButton btncreatenewtask = new JButton ("New Task");
-		JButton btnedittask = new JButton("Edit Task");
-		JButton btndeletetask = new JButton ("Delete Task");
-		JComboBox comboincompletedtask = new JComboBox ();
-		JComboBox combocompletedtask = new JComboBox ();
+		JButton btnCreateNewTask = new JButton ("New Task");
+		JButton btnEditTask = new JButton("Edit Task");
+		JButton btnDeleteTask = new JButton ("Delete Task");
+
+		labelBtnPanel.add(btnCreateNewTask);
+		labelBtnPanel.add(btnEditTask);
+		labelBtnPanel.add(btnDeleteTask);
 		
-		labelcombobtnpanel.add(comboincompletedtask);
-		labelcombobtnpanel.add(combocompletedtask);
-		labelcombobtnpanel.add(btncreatenewtask);
-		labelcombobtnpanel.add(btnedittask);
-		labelcombobtnpanel.add(btndeletetask);
-		
-		add (labelcombobtnpanel, BorderLayout.CENTER);
+		add (labelBtnPanel, BorderLayout.CENTER);
 	}
-	
-	public void AddLabels (){
-		labelpanel.setSize(20, 200);
-		labelpanel.setPreferredSize(new Dimension(20, 200));
-		
-		JLabel lblincomplete = new JLabel ("Incompleted task");
-		JLabel lblcompletedtask = new JLabel ("Completed task");
-		JComboBox comboincompletedtask = new JComboBox ();
-		JComboBox combocompletedtask = new JComboBox ();
-		
-		labelpanel.add(lblincomplete);
-		labelpanel.add(lblcompletedtask);
-	
-		add (labelpanel, BorderLayout.NORTH);
-		
-	}
+
 	
 	public void AddTextArea (){
-		labelinputpanel.setSize(700, 300);
-		labelinputpanel.setPreferredSize(new Dimension(700, 300));
+		JPanel labelInputPanel = new JPanel (new GridLayout(1,1));
+		labelInputPanel.setSize(700, 300);
+		labelInputPanel.setPreferredSize(new Dimension(700, 300));
+		labelInputPanel.add(textDescription);
 		
-		JTextArea textdescription = new JTextArea();
-		labelinputpanel.add(textdescription);
-		add (labelinputpanel, BorderLayout.SOUTH);
+		add (labelInputPanel, BorderLayout.SOUTH);
 	}
 	
 
