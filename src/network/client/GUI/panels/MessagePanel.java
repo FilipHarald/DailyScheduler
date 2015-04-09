236dec7f4a611d3e8a394a0c4a167abe30628867
frenorell <f_norell@hotmail.com>
2015-04-09 16:15:21 +0200

    GUI MessagePanel

--- src/network/client/GUI/panels/MessagePanel.java
+++ src/network/client/GUI/panels/MessagePanel.java
@@ -1,41 +1,63 @@
 package network.client.GUI.panels;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class MessagePanel extends JPanel  {
 	
 	private JButton newMessage = new JButton("New Message");
 	private JButton edit = new JButton ("Edit");
 	private JButton delete = new JButton ("Delete");
-	private JPanel centerPanel = new JPanel();
-//	private JOPtion;
+	private JPanel textPanel = new JPanel();
+	private JPanel buttonsPanel= new JPanel();
+	private JTextArea txt = new JTextArea();
+//	private String[] theOptions = new String[]{"A", "B", "C"};
+//	private JComboBox theBox  = new JComboBox(theOptions);
+	
 
+	
 	public MessagePanel(){
 		super();
+		
 		setBorder(BorderFactory.createTitledBorder("Message view"));
 		
 		//Sizes
-//		centerPanel.setSize(100,100);
-		newMessage.setSize(30,60);
-		edit.setSize(30,60);
-		delete.setSize(30,60);
+		
+		txt.setPreferredSize(new Dimension(300,300));
+		
+		buttonsPanel.setLayout(new BorderLayout());
+		
+		
+		
+		buttonsPanel.add(newMessage, BorderLayout.NORTH);
+		buttonsPanel.add(delete, BorderLayout.CENTER);
+		buttonsPanel.add(edit, BorderLayout.SOUTH);
+		
+		textPanel.add(txt,BorderLayout.CENTER);
+		
 		
 		//Adding
 		listeners();
-		add(newMessage);
-		add(edit);
-		add(delete);
+		add(buttonsPanel, BorderLayout.WEST);
+		add(textPanel, BorderLayout.CENTER);
 		
 		
-		//settings
-		setVisible(true);
+		
+		
+		
 	}
 	
 	public void listeners(){
