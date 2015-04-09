accc06da68a2f12b1e23d09b467163ebf789a524
FilipHarald <filip.harald@gmail.com>
2015-04-09 14:56:02 +0200

    Tog bort de gamla GUI

--- src/network/client/GUI/GUIDailySchedulerMain.java
+++
@@ -1,9 +0,0 @@
-package network.client.GUI;
-
-public class GUIDailySchedulerMain {
-
-	public static void main (String[]args){
-		GUIDailySchedulerPanel GUIDailySchedulerApp = new GUIDailySchedulerPanel();
-	}
-
-}
