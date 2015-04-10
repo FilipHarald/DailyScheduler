be36041cf3c54c7490c64fa8198698132933c651
Adam <adam.o.henriksson@gmail.com>
2015-04-10 09:25:28 +0200

    tagig bort ofärdiga metoder

--- src/network/server/ServerController.java
+++ src/network/server/ServerController.java
@@ -6,7 +6,7 @@ public class ServerController {
 	
 	public final int port = 1234;
 	private Server server = new Server(port, this);
-	private List<String> allUsers = new ArrayList<String>();
+	
 	
 	
 	
@@ -17,34 +17,10 @@ public class ServerController {
 		frame.pack();
 		frame.setVisible(true);
 		
-		try {
-			if(new File(System.getProperty("user.dir") + "/allUsers.dat").isFile()){
-				ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(
-						new FileInputStream(System.getProperty("user.dir") + "/allUsers.dat")));
-				allUsers = (List<String>) ois.readObject();
-			}
-		} catch (ClassNotFoundException | IOException e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
-	public void userConnected(String user){
-		ui.userConnected(user);
-		refreshConnectedList();
-		
-		if(!allUsers.contains(user))
-			allUsers.add(user);
-		
-		try {
-			FileOutputStream fos = new FileOutputStream(System.getProperty("user.dir") + "/allUsers.dat");
-			BufferedOutputStream bos = new BufferedOutputStream(fos);
-			ObjectOutputStream oos = new ObjectOutputStream(bos);
-			oos.writeObject(allUsers);
-			oos.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	
 	
 	public void checkID(){
 		
@@ -66,9 +42,9 @@ public class ServerController {
 	 * @param obj Object to be sent to all connected clients.
 	 * @throws IOException Thrown due to connection issues.
 	 */
-	public void broadcast(Object obj) throws IOException{
-		server.broadcast(obj);
-	}
+	//public void broadcast(Object obj) throws IOException{
+		//server.broadcast(obj);
+	//}
 	
 	public static void main(String[] args){
 		ServerController controller = new ServerController();
