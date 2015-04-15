c3063d2292ae2c9a7bdf5cc05cf7a2eab186a59d
FilipHarald <Filip.harald@gmail.com>
2015-04-15 19:34:12 +0200

    Coninued with the method saveEntity

--- src/database/Database.java
+++ src/database/Database.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -29,14 +30,14 @@ import entities.User;
 import entities.Task;
 
 public class Database {
-	
+
 	private static Connection connection;
 	private User user;
 	private Team team;
 	private Task task;
 	private DatabasGUI gui = new DatabasGUI();
-	
-	public Database(){
+
+	public Database() {
 		try {
 			connectToMySql();
 		} catch (ClassNotFoundException e) {
@@ -44,209 +45,187 @@ public class Database {
 			e.printStackTrace();
 		}
 	}
-	
-	//Start connection to localhost
-	public void connectToMySql () throws ClassNotFoundException{
+
+	// Start connection to localhost
+	public void connectToMySql() throws ClassNotFoundException {
 		String host = "jdbc:mysql://localhost/test";
 		String username = "root", password = "";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(host, username, password);
-			System.out.println ("Connected to database");
+			System.out.println("Connected to database");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public void saveEntity(Object obj) throws SQLException {
-		System.out.println("saveEntity");
-		
 		PreparedStatement prepStatement;
-		if(obj instanceof User){
-			User user = (User)obj;
-			
-			if(user.getId()==0){
-				prepStatement = connection.prepareStatement("INSERT INTO User (Name, Password) values (?, ?)");
+
+		if (obj instanceof User) {
+			User user = (User) obj;
+			if (user.getId() == 0) {
+				prepStatement = connection
+						.prepareStatement("INSERT INTO User (Name, Password) values (?, ?)");
 				prepStatement.setString(1, user.getName());
 				prepStatement.setString(2, user.getPassword());
 				insertToTable(prepStatement);
-				if(user.isAdmin()){
-//					insertToTable(String.format("INSERT INTO Admin (UserId) values (%s)", user.getId()));
+				if (user.isAdmin()) {
+					prepStatement = connection
+							.prepareStatement("INSERT INTO Admin (User) values (?)");
+					prepStatement.setBoolean(1, true);
+					insertToTable(prepStatement);
 				}
-			}else{
-//				updateTable("User", user.getId(), user.getName(), user.getPassword());
+			} else {
+				// updateTable("User", user.getId(), user.getName(),
+				// user.getPassword());
 			}
-		}else if(obj instanceof Task){
-			
+		} else if (obj instanceof Task) {
+			Task task = (Task) obj;
+			if (task.getId() == 0) {
+				prepStatement = connection
+						.prepareStatement("INSERT INTO Task (Date, Author, Description) values (?,?,?)");
+				prepStatement.setDate(1, (Date) task.getDate());
+				prepStatement.setInt(2, task.getAuthor());
+				prepStatement.setString(3, task.getDescription());
+				insertToTable(prepStatement);
+			} else {
+				// updateTable
+			}
+		} else if (obj instanceof Team) {
+			if (task.getId() == 0) {
+				Team team = (Team) obj;
+				prepStatement = connection
+						.prepareStatement("INSERT INTO Team (Name) values (?)");
+				prepStatement.setString(1, team.getName());
+			} else {
+				// updateTable
+			}
 		}
 	}
-	
+
 	private void insertToTable(PreparedStatement statement) {
-		
-		System.out.println();
 		try {
 			statement.executeUpdate();
 			statement.close();
 			connection.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		}
+	}
 
-	//Send data to table user
-	public void sendToTableUser (int idtable_user, String name, String title) throws SQLException {
-		PreparedStatement state = connection.prepareStatement("INSERT INTO table_user (idtable_user, name, title) values (?,?,?)");
-		
-		state.setInt(1, idtable_user);
-		state.setString(2, name);
-		state.setString(3, title);
-		
-		state.executeUpdate();
-		state.close();
-		connection.close();
-		System.out.println("Saved to table user");
-		
-	}
-	
-	//Send data to table task
-	public void sendToTableTask (int idtable_task, String name, String text) throws SQLException{
-		PreparedStatement state = connection.prepareStatement("INSERT INTO table_task (idtable_task, name, text) values (?,?,?)");
-		
-
-		state.setInt(1, idtable_task);
-		state.setString(2, name);
-		state.setString(3, text);
-	
-		state.executeUpdate();
-		state.close();
-		connection.close();
-		
-		System.out.println ("Saved to table task");
-		
-	}
-	
-	//Send data to table teams
-	public void sendToTableTeams (int idtable_teams, String name) throws SQLException{
-		PreparedStatement state = connection.prepareStatement("Insert into table_teams (idtable_teams, name) values (?,?)");
-		
-		state.setInt(1, idtable_teams);
-		state.setString(2, name);
-		state.executeUpdate();
-		state.close();
-		connection.close();
-		
-	}
-	
-	//Get users table
-	public static ResultSet getUsersResult (Connection connect1, String sql) throws Exception {
-		Statement state = (Statement) connect1.createStatement(java.sql.ResultSet.CONCUR_READ_ONLY, java.sql.ResultSet.TYPE_FORWARD_ONLY);
+	// Get users table
+	public static ResultSet getUsersResult(Connection connect1, String sql)
+			throws Exception {
+		Statement state = (Statement) connect1.createStatement(
+				java.sql.ResultSet.CONCUR_READ_ONLY,
+				java.sql.ResultSet.TYPE_FORWARD_ONLY);
 		return state.executeQuery(sql);
-		
+
 	}
-	
-	public static ResultSet getUsers (Connection connect2, String tableName) throws Exception{
+
+	public static ResultSet getUsers(Connection connect2, String tableName)
+			throws Exception {
 		String sqlStatement = String.format("select * from %s", tableName);
-		return getUsersResult (connect2, sqlStatement );
-		
+		return getUsersResult(connect2, sqlStatement);
+
 	}
-	
-	//Get team table
-	public static ResultSet getTeamResult (Connection connection, String sql) throws SQLException{
-		Statement state = (Statement) connection.createStatement(java.sql.ResultSet.CONCUR_READ_ONLY, java.sql.ResultSet.TYPE_FORWARD_ONLY);
+
+	// Get team table
+	public static ResultSet getTeamResult(Connection connection, String sql)
+			throws SQLException {
+		Statement state = (Statement) connection.createStatement(
+				java.sql.ResultSet.CONCUR_READ_ONLY,
+				java.sql.ResultSet.TYPE_FORWARD_ONLY);
 		return state.executeQuery(sql);
 	}
-	
-	public static ResultSet getTeams (Connection connection) throws SQLException {
+
+	public static ResultSet getTeams(Connection connection) throws SQLException {
 		return getTeamResult(connection, "select * from Teams");
-		
+
 	}
-	
-	//Get task table
-	public static ResultSet getTaskResult (Connection connection, String sql) throws SQLException {
-		Statement state = (Statement) connection.createStatement(java.sql.ResultSet.CONCUR_READ_ONLY, java.sql.ResultSet.TYPE_FORWARD_ONLY);
+
+	// Get task table
+	public static ResultSet getTaskResult(Connection connection, String sql)
+			throws SQLException {
+		Statement state = (Statement) connection.createStatement(
+				java.sql.ResultSet.CONCUR_READ_ONLY,
+				java.sql.ResultSet.TYPE_FORWARD_ONLY);
 		return state.executeQuery(sql);
 	}
-	
-	public static ResultSet getTask (Connection connection) throws SQLException{
+
+	public static ResultSet getTask(Connection connection) throws SQLException {
 		return getTeamResult(connection, "select * from Task");
 	}
-	
-	//Shows the information stored in databases 
-	public void ShowResult () throws Exception{
+
+	// Shows the information stored in databases
+	public void ShowResult() throws Exception {
 		JOptionPane.showMessageDialog(null, "Visa information från databas");
-		JOptionPane.showMessageDialog(null, "User"+ "\n"+ "Team"+ "\n"+ "Task");
+		JOptionPane.showMessageDialog(null, "User" + "\n" + "Team" + "\n"
+				+ "Task");
 		String pane = JOptionPane.showInputDialog("");
 
-		
 		ResultSet rs = getUsers(connection, pane);
-		while (rs.next()){
-			
-			System.out.println( rs.getString(1)+ ", "+ rs.getString(2)+ ", "+ rs.getString(3));
-		
+		while (rs.next()) {
+
+			System.out.println(rs.getString(1) + ", " + rs.getString(2) + ", "
+					+ rs.getString(3));
+
 		}
-//		if (pane.equals("User")){
-//		ResultSet rs = getUsers(connect, "User");
-//		while (rs.next()){
-//			System.out.println( rs.getString(1)+ ", "+ rs.getString(2)+ ", "+ rs.getString(3));
-//		
-//		}
-//		}else if (pane.equals("Team")){
-//		ResultSet rs2 = getTeams (connect);
-//		while (rs2.next()){	
-//			System.out.println(rs2.getString(1)+ ", "+ rs2.getString(2));
-//		}
-//
-//		} else if (pane.equals("Task")){
-//			ResultSet rs3 = getTask(connect);
-//			while (rs3.next()){
-//				System.out.println (rs3.getString(1)+ ", "+ rs3.getString(2)+ ", "+ rs3.getString(3));
-//			}
-//			
-//		}
+		// if (pane.equals("User")){
+		// ResultSet rs = getUsers(connect, "User");
+		// while (rs.next()){
+		// System.out.println( rs.getString(1)+ ", "+ rs.getString(2)+ ", "+
+		// rs.getString(3));
+		//
+		// }
+		// }else if (pane.equals("Team")){
+		// ResultSet rs2 = getTeams (connect);
+		// while (rs2.next()){
+		// System.out.println(rs2.getString(1)+ ", "+ rs2.getString(2));
+		// }
+		//
+		// } else if (pane.equals("Task")){
+		// ResultSet rs3 = getTask(connect);
+		// while (rs3.next()){
+		// System.out.println (rs3.getString(1)+ ", "+ rs3.getString(2)+ ", "+
+		// rs3.getString(3));
+		// }
+		//
+		// }
 	}
 
+	// public void SaveResult (){
+	//
+	// JOptionPane.showMessageDialog(null, "Spara till databas");
+	// JOptionPane.showMessageDialog(null, "User"+ "\n"+ "Team"+ "\n"+ "Task");
+	// String pane = JOptionPane.showInputDialog("");
+	// try {
+	// saveEntity(new User ("Testa Karlsson", false, "1234", 0));
+	// } catch (SQLException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	// if (pane.equals("User")){
+	// try {
+	// sendToTableUser(gui.getid(), gui.getName(), gui.getTitle());
+	// } catch (SQLException e) {
+	// e.printStackTrace();
+	// }
+	// } else if (pane.equals("Team")){
+	// try {
+	// sendToTableTeams (gui.getid(), gui.getName());
+	// } catch (SQLException e){
+	// e.printStackTrace();
+	// }
+	//
+	// }else if (pane.equals("Task")){
+	// try {
+	// sendToTableTask (gui.getid(), gui.getName(), gui.getTitle());
+	// } catch (SQLException e) {
+	// e.printStackTrace();
+	// }
+	// }
 
-//	public void SaveResult (){
-//		
-//		JOptionPane.showMessageDialog(null, "Spara till databas");
-//		JOptionPane.showMessageDialog(null, "User"+ "\n"+ "Team"+ "\n"+ "Task");
-//		String pane = JOptionPane.showInputDialog("");
-//		try {
-//			saveEntity(new User ("Testa Karlsson", false, "1234", 0));
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		if (pane.equals("User")){
-//			try {
-//				sendToTableUser(gui.getid(), gui.getName(), gui.getTitle());
-//				} catch (SQLException e) {
-//				e.printStackTrace();
-//				}
-//		} else if (pane.equals("Team")){
-//			try {
-//				sendToTableTeams (gui.getid(), gui.getName());
-//			} catch (SQLException e){
-//				e.printStackTrace();
-//			}
-//			
-//		}else if (pane.equals("Task")){
-//			try {
-//				sendToTableTask (gui.getid(), gui.getName(), gui.getTitle());
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			}
-//		}
-		
-	
-	}
-
-	
-
-
-
-
-
+}
