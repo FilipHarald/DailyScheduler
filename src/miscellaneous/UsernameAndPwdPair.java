021f1c9d259062aa54d826c37e15a3bce426d146
FilipHarald <Filip.harald@gmail.com>
2015-04-16 08:25:06 +0200

    Changed Autentication to a more appropriate name

---
+++ src/miscellaneous/UsernameAndPwdPair.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miscellaneous;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Aya
+ */
+public class UsernameAndPwdPair implements Serializable {
+	private String userName;
+	private char[] password;
+
+	public UsernameAndPwdPair(String userName, char[] password) {
+		this.userName = userName;
+		this.password = password;
+
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public char[] getPassword() {
+		return password;
+	}
+
+}
