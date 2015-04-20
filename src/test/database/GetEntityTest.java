1a326f6472764ea6dde0a09409f55edc046c3699
henkeklein <henrikklein@hotmail.com>
2015-04-20 08:36:21 +0200

    Ändrat i JUnit testet

--- src/test/database/GetEntityTest.java
+++ src/test/database/GetEntityTest.java
@@ -27,19 +27,19 @@ public class GetEntityTest {
 		try {
 			user = (User) db.getEntity("User", 5);
 			task = (Task) db.getEntity("Task", 1);
-//			team = (Team) db.getEntity("Team", 1);
+			team = (Team) db.getEntity("Team", 1);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		assertEquals("UserObj", "Testa Karlsson", user.getName());
 		assertEquals("TaskObj", 1, task.getAuthor());
+		assertEquals("TeamObj", 1, team.getId());
 		String [] str = task.getSubtasks();
 		for (String s : str){
 			System.out.println(s);
 		}
 //		assertEquals("TaskObj", 1, task.getSubtasks());
 		
-//		assertEquals("TeamObj", "lagett", team.getName());
 	}
 
 }
