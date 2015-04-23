acf00574d684291183904f78aa887252d16d280b
ayaabutaleb <abutalebaya@gmail.com>
2015-04-23 12:33:50 +0200

    added onClick and method to view list of events in JList

--- src/network/client/GUI/panels/EventPanel.java
+++ src/network/client/GUI/panels/EventPanel.java
@@ -6,35 +6,43 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.List;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import network.client.controllers.EventController;
 
 public class EventPanel extends JPanel implements ActionListener {
+
     private EventController ec;
-    
+    private Event event;
+
     private JDateChooser jdcDate = new JDateChooser();
-    
+
     private JList lstEvents = new JList();
-    
+
     private JFrame frmNewEvent = new JFrame("New event");
     private JFrame frmEditEvent = new JFrame("Edit event");
-    private JFrame frmSearchEvent = new JFrame("Search event"); 
+    private JFrame frmSearchEvent = new JFrame("Search event");
 
     private JButton btnAddEvent = new JButton("Add new event");
     private JButton btnEditEvent = new JButton("Edit event");
     private JButton btnDeleteEvent = new JButton("Delete");
     private JButton btnSaveEvent = new JButton("Save");
-    private JButton btnSearch = new JButton ("Search");
+    private JButton btnSearch = new JButton("Search");
 
     private JLabel lblDescription = new JLabel("Title");
     private JLabel lblDate = new JLabel("Date");
     private JLabel lblParticipants = new JLabel("Participants");
     private JLabel lblSearch = new JLabel("Type the eventID you wish to search for");
-    
+
     private JTextField tfDescription = new JTextField(15);
     private JTextField tfDate = new JTextField(15);
     private JTextArea taParticipants = new JTextArea();
@@ -51,7 +59,7 @@ public class EventPanel extends JPanel implements ActionListener {
         pnlButton.setLayout(new BorderLayout());
         pnlButton.add(btnAddEvent, BorderLayout.NORTH);
         pnlButton.add(btnEditEvent, BorderLayout.CENTER);
-        
+
         add(pnlButton, BorderLayout.CENTER);
 
         pnlNewEvent.setLayout(new BorderLayout());
@@ -66,6 +74,7 @@ public class EventPanel extends JPanel implements ActionListener {
         btnDeleteEvent.addActionListener(this);
         btnSaveEvent.addActionListener(this);
         btnSearch.addActionListener(this);
+        lstEvents.addMouseListener(mouseEvent);
 
     }
 
@@ -121,7 +130,7 @@ public class EventPanel extends JPanel implements ActionListener {
 
         btnSaveEvent.setBounds(175, 250, 100, 25);
         btnDeleteEvent.setBounds(340, 250, 100, 25);
-        
+
         pnlEditEvent.add(lblDescription);
         pnlEditEvent.add(tfDescription);
         pnlEditEvent.add(lblDate);
@@ -131,83 +140,84 @@ public class EventPanel extends JPanel implements ActionListener {
         pnlEditEvent.add(taParticipants);
         pnlEditEvent.add(btnSaveEvent);
         pnlEditEvent.add(btnDeleteEvent);
-        
+
         frmEditEvent.add(pnlEditEvent);
         frmEditEvent.setLocationRelativeTo(null);
         frmEditEvent.pack();
         frmEditEvent.setVisible(true);
 
     }
-    
-    private void renderSearchEvent(){
+
+    private void renderSearchEvent() {
         frmSearchEvent.setLayout(null);
         frmSearchEvent.setMinimumSize(new Dimension(500, 450));
-        
+
         pnlSearchEvent.setLayout(null);
         pnlSearchEvent.setSize(500, 450);
-        
+
         lblSearch.setBounds(120, 50, 300, 30);
         lstEvents.setBounds(135, 90, 200, 250);
         btnSearch.setBounds(370, 90, 80, 30);
-        
+
         pnlSearchEvent.add(lblSearch);
         pnlSearchEvent.add(lstEvents);
         pnlSearchEvent.add(btnSearch);
-        
+
         frmSearchEvent.add(pnlSearchEvent);
         frmSearchEvent.setLocationRelativeTo(null);
         frmSearchEvent.pack();
         frmSearchEvent.setVisible(true);
-        
+
+        displayEventList(event);
+
     }
-    
-    
-    
-    private void clearFields(){
+
+    private void clearFields() {
         tfDescription.setText(null);
         jdcDate.setDate(null);
         taParticipants.setText(null);
-        
+
     }
-    private void setLabels(){
+
+    private void setLabels() {
         lblDescription.setText("Title");
         lblDate.setText("Date");
         lblParticipants.setText("Participants");
-                
+
     }
-    
-    private boolean isEmpty(){
+
+    private boolean isEmpty() {
         boolean isEmpty = false;
-        
-        if(tfDescription.getText().trim().isEmpty()){
+
+        if (tfDescription.getText().trim().isEmpty()) {
             lblDescription.setText("Title *");
-            isEmpty= true;
-            
-        } 
-        if (jdcDate.getDate() == null){
+            isEmpty = true;
+
+        }
+        if (jdcDate.getDate() == null) {
             lblDate.setText("Date *");
-            isEmpty= true;
-            
+            isEmpty = true;
+
         }
-               
-        if(taParticipants.getText().trim().isEmpty()){
+
+        if (taParticipants.getText().trim().isEmpty()) {
             lblParticipants.setText("Participants *");
-            isEmpty= true;
+            isEmpty = true;
         }
-        
+
         return isEmpty;
-        
-    } 
-    
-    public String getDescription(){
+
+    }
+
+    public String getDescription() {
         return tfDescription.getText();
     }
-    
-    public Date getDate(){
+
+    public Date getDate() {
         return jdcDate.getDate();
     }
-    
-    public String getParticipants(){
+
+    public String getParticipants() {
         return taParticipants.getText();
     }
 
@@ -218,22 +228,23 @@ public class EventPanel extends JPanel implements ActionListener {
             setLabels();
             clearFields();
             renderNewEvent();
-                    
+
         } else if ((button) == btnEditEvent) {
             //TODO: add display event
             renderSearchEvent();
-            DefaultListModel model = new DefaultListModel();
-            for(){
-                model.addElement(i);
+
+        } else if ((button) == btnSearch) {
+            try{
+            String s = (String) lstEvents.getSelectedValue().toString();
+            
+                frmSearchEvent.dispose();
+                setLabels();
+                clearFields();
+                tfDescription.setText(s);
+                renderEditEvent();
+            }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, "Please select an event" );
             }
-            lstEvents.setModel(model);
-            
-            
-        } else if((button) == btnSearch){
-            frmSearchEvent.dispose();
-            setLabels();
-            clearFields();
-            renderEditEvent();
 
         } else if ((button) == btnDeleteEvent) {
             //JOptionPane.showMessageDialog(null, "Do you wish to delete event: " + tfTitle.getText() + "?");
@@ -245,36 +256,57 @@ public class EventPanel extends JPanel implements ActionListener {
                 frmEditEvent.dispose();
             } else {
                 JOptionPane.showMessageDialog(null, "The event will not be deleted");
-                
+
             }
-            
+
         } else if ((button) == btnSaveEvent) {
             setLabels();
-            if(isEmpty() == true){
+            if (isEmpty() == true) {
                 JOptionPane.showMessageDialog(null, "Please fill in all of the fields marked with an astrerisk");
             } else {
                 //TODO: save event to database
                 frmNewEvent.dispose();
                 frmEditEvent.dispose();
             }
-            
+
         }
 
     }
+
+    MouseListener mouseEvent = new MouseAdapter() {
+        public void mouseClicked(MouseEvent e) {
+            JList lstEvent = (JList) e.getSource();
+            if (e.getClickCount() == 2) {
+
+                String selected = (String) lstEvent.getSelectedValue().toString();
+
+                frmSearchEvent.dispose();
+                setLabels();
+                clearFields();
+                tfDescription.setText(selected);
+                renderEditEvent();
+            }
+        }
+    };
+
 //    public void getTitle(){
 //        
 //    }
-    public void displayEventList(){
-        
-    }
-
     public void displayEvent(Event event) {
         tfDescription.setText(event.getDescription());
         jdcDate.setDate(event.getDate());
         taParticipants.setText(event.getParticipants(null).toString());
-        
 
     }
 
-  
+    private void displayEventList(Event event) {
+        DefaultListModel model = new DefaultListModel();
+        for (int i = 0; i < 5; i++) {
+            model.addElement(i);
+
+        }
+        lstEvents.setModel(model);
+//        lstEvents.setModel((ListModel)ec.displayEventList(event));
+    }
+
 }
