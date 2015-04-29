45e46544360f4fc987892d0bb13f5a0b10b36b50
FilipHarald <filip.harald@gmail.com>
2015-04-29 10:53:53 +0200

    Message toString

--- src/entities/Message.java
+++ src/entities/Message.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+' * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -55,5 +55,8 @@ public class Message implements Serializable {
     public void setRecipients(ArrayList<String> string){
         this.recipients = string;
         
+    }
+    public String toString(){
+    	return title;
     }
 }
