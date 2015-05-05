681ce14d46fd7a85c6be17b2f8c5a1ad4c53b614
ayaabutaleb <abutalebaya@gmail.com>
2015-05-05 14:21:04 +0200

    lista visas i GUI

--- src/entities/Message.java
+++ src/entities/Message.java
@@ -1,5 +1,5 @@
 /*
-' * To change this license header, choose License Headers in Project Properties.
+ ' * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -13,54 +13,60 @@ import java.util.ArrayList;
  * @author Aya
  */
 public class Message implements Serializable {
-	private User user;
-	private String title;
-	private String msg;
-	private int id;
-	private ArrayList<Integer> temp;
 
-	public Message(String title, String msg, int[] recipients, int id) {
-		this.title = title;
-		this.msg = msg;
-		this.id = id;
-		ArrayList<Integer> temp = new ArrayList<Integer>();
-		for (int r : recipients) {
-			temp.add(r);
-		}
+    private User user;
+    private String title;
+    private String msg;
+    private int id;
+    private ArrayList<Integer> recipients;
 
-	}
+    public Message(String title, String msg, int[] recipients, int id) {
+        this.title = title;
+        this.msg = msg;
+        this.id = id;
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        if (recipients != null) {
+            for (int r : recipients) {
+                temp.add(r);
+            }
+        }
+        this.recipients = temp;
 
-	public int getId() {
-		return id;
-	}
+        }
 
-	public String getTitle() {
-		return title;
-	}
+	
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getMessage() {
-		return msg;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setMessage(String string) {
-		this.msg = msg;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public ArrayList<Integer> getRecipients() {
-		return temp;
-	}
+    public String getMessage() {
+        return msg;
+    }
 
-	public void setRecipients(Integer[] recipients) {
-		for (Integer r : recipients) {
-			temp.add(r);
-		}
-	}
+    public void setMessage(String string) {
+        this.msg = msg;
+    }
 
-	public String toString() {
-		return title;
-	}
+    public ArrayList<Integer> getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(Integer[] recipients) {
+        for (Integer r : recipients) {
+           this.recipients.add(r);
+        }
+    }
+
+    public String toString() {
+        return title;
+    }
 }
