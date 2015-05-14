1fba003a0aa9c42c5e759f0074d3d605b15c69d2
ayaabutaleb <abutalebaya@gmail.com>
2015-05-14 13:08:53 +0200

    delete event works

--- src/miscellaneous/DeleteMe.java
+++ src/miscellaneous/DeleteMe.java
@@ -1,14 +1,17 @@
 package miscellaneous;
+
+import java.io.Serializable;
+
 /**
  * This class is intented to seprate the delete-method from the insert- and update-method in the DatabaseController.
  * @author Filip
  *
  */
-public class DeleteMe {
+public class DeleteMe implements Serializable {
 	private Object objectToDelete;
 
 	public DeleteMe(Object objToDelete){
-		this.objectToDelete = objectToDelete;
+		this.objectToDelete = objToDelete;
 	}
 	public Object getObject() {
 		return objectToDelete;
